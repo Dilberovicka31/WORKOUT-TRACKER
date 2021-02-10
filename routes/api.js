a4f055005621d0930eba8526955e389cb84081f7
Dilberovicka31 <dilberovicm36@gmail.com>
2021-02-09 21:45:19 -0800

    added put route

--- routes/api.js
+++ routes/api.js
@@ -1,36 +1,41 @@
 const router = require("express").Router();
 
-const {Workout} = require("../models");
+const { Workout } = require("../models");
 
-router.get("/api/workouts", (req,res) =>{
-
-Workout.find().then((result) =>{
-    res.json(result)
-}).catch((err)=>{
-    res.status(500).json(err);
-})
-
-});
-router.put("/api/workouts/:id", (req,res) =>{
-    const updateWorkout = req.body
-    Workout.findByIdAndUpdate(req.params.id,{ $push})
-
-});
-
-router.post("/api/workouts", (req,res) =>{
-
-    
-    Workout.create(req.body).then((result)=>{
-        res.json(result);
-    }).catch((err) =>{
-        err.status(500).json(err);
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((result) => {
+      res.json(result);
     })
-
-
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+router.put("/api/workouts/:id", (req, res) => {
+  const updateWorkout = req.body;
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: updateWorkout } },
+    { new: true }
+  )
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
-router.get("/api/workouts/range", (req,res) =>{
-
+router.post("/api/workouts", (req, res) => {
+  Workout.create()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      err.status(500).json(err);
+    });
 });
+
+router.get("/api/workouts/range", (req, res) => {});
 
 module.exports = router;
