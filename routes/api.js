24b3c779c9d93e711a8383dd5c8ac81c005b7406
Dilberovicka31 <dilberovicm36@gmail.com>
2021-02-09 20:47:28 -0800

    created routes post and get

--- routes/api.js
+++ routes/api.js
@@ -4,20 +4,24 @@ const {Workout} = require("../models");
 
 router.get("/api/workouts", (req,res) =>{
 
-Workout.find().then((workouts) =>{
-    res.json(workouts)
+Workout.find().then((result) =>{
+    res.json(result)
 }).catch((err)=>{
     res.status(500).json(err);
 })
 
 });
 router.put("/api/workouts/:id", (req,res) =>{
+    const updateWorkout = req.body
+    Workout.findByIdAndUpdate(req.params.id,{ $push})
 
 });
 
 router.post("/api/workouts", (req,res) =>{
-    Workout.create(req.body).then((workout)=>{
-        res.json(workout);
+
+    
+    Workout.create(req.body).then((result)=>{
+        res.json(result);
     }).catch((err) =>{
         err.status(500).json(err);
     })
--- server.js
+++ server.js
@@ -21,7 +21,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gentle-badlands
   useFindAndModify: false,
 });
 
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT} !`);
